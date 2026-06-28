15020e22814be9ab499f5c8e4ed66c824454442c
JanhaviAkalwadi12 <januakalwadi@gmail.com>
2026-06-28 22:58:41 +0530

    Fixed build issues

---
+++ dist/server/assets/_tanstack-start-manifest_v-C_sqsqXX.js
@@ -0,0 +1,13 @@
+//#region \0tanstack-start-manifest:v
+var tsrStartManifest = () => ({ routes: { __root__: {
+	filePath: "C:/Users/hp/Downloads/Mayur akalwadi portfolio site/src/routes/__root.tsx",
+	children: [],
+	preloads: ["/assets/index-BCEoJHdg.js"],
+	scripts: [{ attrs: {
+		type: "module",
+		async: !0,
+		src: "/assets/index-BCEoJHdg.js"
+	} }]
+} } });
+//#endregion
+export { tsrStartManifest };
